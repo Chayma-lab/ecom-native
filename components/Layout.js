fa79fde0f939613e700175cb782748eb64675b09
Chayma <114072354+Chayma-lab@users.noreply.github.com>
2025-05-05 19:54:39 +0200

    commentaar layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,18 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
 
+// ******************* COMPONENT *******************
+// ******************* COMPONENT *******************
+// ******************* COMPONENT *******************
 export default function Layout({ children, style }) {
   return (
     <SafeAreaView style={[styles.container, style]}>{children}</SafeAreaView>
   );
 }
 
+// ******************* STYLES *******************
+// ******************* STYLES *******************
+// ******************* STYLES *******************
 const styles = StyleSheet.create({
   container: {
     flex: 1,
