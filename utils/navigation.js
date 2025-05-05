c2b73e4ad8d383782ebd6de8ca79acbfaec3f8d0
Chayma <114072354+Chayma-lab@users.noreply.github.com>
2025-05-05 20:06:23 +0200

    code cleanup

--- utils/navigation.js
+++ utils/navigation.js
@@ -2,15 +2,10 @@ import { createNavigationContainerRef } from "@react-navigation/native";
 
 export const navigationRef = createNavigationContainerRef();
 
-/**
- * Slimme navigate functie
- * @param {string} screen - Naam van het scherm
- * @param {object} params - Optionele parameters
- */
 export function navigateTo(screen, params = {}) {
   if (!navigationRef.isReady()) return;
 
-  const tabScreens = ["Home", "Cart"]; // Voeg hier je tab screens toe
+  const tabScreens = ["Home", "Cart"];
 
   if (tabScreens.includes(screen)) {
     navigationRef.navigate("Tabs", {
